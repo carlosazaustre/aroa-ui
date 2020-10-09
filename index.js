b5d1cf32f083f3fc01e2fd8df285c6d90e086026
Carlos Azaustre <cazaustre@gmail.com>
2020-10-09 15:53:27 +0200

    add action to release

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// TODO:
+// Atoms
+export { default as Heading } from './lib/components/atoms/Heading'
+export { default as Icon } from './lib/components/atoms/Icon'
+export { default as Input } from './lib/components/atoms/Input'
+export { default as Label } from './lib/components/atoms/Label'
+export { default as Link } from './lib/components/atoms/Link'
+export { default as Picture } from './lib/components/atoms/Picture'
+export { default as Text } from './lib/components/atoms/Text'
+
+// Layout
+export { default as Spacer } from './lib/components/layout/Spacer'
+
+// Molecules
+export { default as Avatar } from './lib/components/molecules/Avatar'
+export { default as Button } from './lib/components/molecules/Button'
